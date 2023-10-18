f0d7bb529a747c6c8a5dcff613b8287f0c716e93
Ridhima10 <ridhimajain862@gmail.com>
2023-10-18 15:03:39 +0530

    undefined and not defined examples

---
+++ 4. Javascript section/undefined vs not defined.js
@@ -0,0 +1,42 @@
+// var a; //js will automatically allot memory to a as undefined bcz it is in the first phase now
+
+
+
+// var a = 7;
+// console.log(x); //not defined bcz x is not present
+
+
+// console.log(a); //undefined bcz of hoisting
+// a = 7;
+// console.log(x) //not defined
+
+
+
+// console.log(a); //undefined bcz of hoising 
+// var a = 10;
+// console.log(a); //10
+
+
+// var a;
+// console.log(a);
+// if (a === undefined) {
+//     console.log("it is undefined");
+// }
+// else {
+//     console.log("It is not undefined");
+// }
+
+
+// it is loosely typed language which means we dont need to specify datatype while initializing
+var a;
+console.log(a)
+a = 10;
+console.log(a)
+a = "hello";
+console.log(a)
+
+// also known as weakly typed language
+
+
+a=undefined //it is not a good practice to assign undefined to a var becuase it has a special meaning moreover it will automatically assign undefined to a variable during the first phase so there is no need to assign.
+
