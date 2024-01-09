8318fee954b527efbb29755bb68606bb149dd7c5
Ridhima10 <ridhimajain862@gmail.com>
2024-01-09 14:13:29 +0530

    added summary of  'THINGS LEARNED'

--- 4. Javascript New/setTimeout and closure interview questons.js
+++ 4. Javascript New/setTimeout and closure interview questons.js
@@ -17,8 +17,12 @@ setTimeout(function() {
 console.log("World");
 /*In this example, "Hello" is logged first, then "World," and after a delay of 2000 milliseconds (2 seconds), "Delayed message" is logged. The setTimeout function allows you to introduce non-blocking delays in your code.*/
 
-
-
+/*
+Things learned:
+1. setTimeout stores the function in a different place and attached a timer to it, when the timer is finished it rejoins the call stack and executed.
+2. Without closure the var reference gives the latest value as it does not retain the original value but rather has the reference so any update in value after timeout will be shown.
+3. If we use let/const because they have block scope, every time a new copy of variable is attached, thus this can be done without closure.
+*/
 
 
 
