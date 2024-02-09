8a27a474f845cf67a3c5fbeecb2859dfb8b89c6c
Ridhima10 <ridhimajain862@gmail.com>
2024-02-09 15:44:35 +0530

    Inserting and removing elements

--- 4. Javascript New/Inserting and removing elements in js.js
+++ 4. Javascript New/Inserting and removing elements in js.js
@@ -39,7 +39,43 @@ console.log(document.querySelector(".box").textContent)
 document.querySelector(".box").innerHTML ="Changed HTML"
 
 
-// has attribute
+// has attribute- checks existence of an element
+ console.log(document.querySelector(".box").hasAttribute("style")) //returns boolean
+
+
+console.log(document.querySelector(".box").getAttribute("style")) //returns value
+
+
+// changes the attribute
+console.log(document.querySelector(".box").setAttribute("style","color:red")) //returns value
+
+
+// to fetch all attributes
+console.log(document.querySelector(".box").attributes)
+
+
+// removes the atttribute targetted css
+console.log(document.querySelector(".box").removeAttribute("style"))
+
+
+// console.log(document.designMode="on")
+
+
+// data attributes
+console.log(document.querySelector(".box").dataset)
+
+
+
+// deletion methods
+document.querySelector(".box").remove() //removes first .box
+
+// logs the whole line
+console.log(document.querySelector(".container"))
+console.log(document.querySelector(".container").classList) //list of classes
+console.log(document.querySelector(".container").className) //name of classes
+console.log(document.querySelector(".container").classList.add("pink")) //add new class
+console.log(document.querySelector(".container").classList.remove("pink")) //remove a class
+console.log(document.querySelector(".container").classList.toggle("pink")) //if class is there, then it will remove it and vice versa
 
 
 
