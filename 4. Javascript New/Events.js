1680b94721887a41b996b7c84ce7f14ad1a3ea58
Ridhima10 <ridhimajain862@gmail.com>
2024-02-10 23:21:47 +0530

    eventListeners

---
+++ 4. Javascript New/Events.js
@@ -0,0 +1,16 @@
+let button=document.getElementById("btn")
+// button.addEventListener("dblclick",()=>{
+//     // alert("I was clicked!!!")
+//     document.querySelector(".box").innerHTML="You were also <b>clicked</b>"
+// })
+
+
+// button.addEventListener("contextmenu",()=>{
+//     alert("I was right clicked!!!")
+// })
+
+
+document.addEventListener("keydown",(e)=>{
+    console.log(e.key)
+})
+
