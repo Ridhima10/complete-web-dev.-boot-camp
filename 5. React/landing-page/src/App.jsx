938465a2a3e31db2b4f2399c628e4a20a03cf3f2
Ridhima10 <ridhimajain862@gmail.com>
2024-12-07 13:23:04 +0530

    usestate hook

--- 5. React/landing-page/src/App.jsx
+++ 5. React/landing-page/src/App.jsx
@@ -8,17 +8,22 @@ import Card from './components/card'
 import { flip } from '@popperjs/core'
 
 
+
 function App() {
+  const [count,setcount]=useState(0);
 
   return (
    <>
-   <Navbar/>
+    <Navbar/>
    <div className="d-flex justify-content-center align-items-center flex-wrap">
    <Card desc={"This is 1st card"} />
    <Card desc={"This is 2nd card"}/>
    <Card desc={"This is 3rd card"}/>
    <Card desc={"This is 4th card"} />
-   
+   </div>
+   <h4 className='text-center mt-4'>The count is {count}</h4>
+   <div className='d-flex justify-center align-items-center'>
+   <button  onClick={()=>setcount(count+1)}  className='btn my-4 text-white btn-info  '>Click me!!!</button>
    </div>
    <Footer/>
    </>
